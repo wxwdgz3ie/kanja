c8702f8c4aa390770f62399cc81165afad3cba80
wxwdgz3ie <70618636+wxwdgz3ie@users.noreply.github.com>
2020-09-02 11:12:29 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\'infolayerM\'>");
+document.writeln("<div class=\'name\'><b>행복약국</b></div>");
+document.writeln("<div class=\'nameDT\'>카톡:KK78</div>");
+document.writeln("<div class=\'info\'><b>!!</b> 비밀배송.당일배송.정품보장. <b>!!</b></div>");
+document.writeln("<div>");
+document.writeln("    <a href=\'index.php\' target=\'_blank\' class=\'confirm\'>사이트로 이동</a>");
